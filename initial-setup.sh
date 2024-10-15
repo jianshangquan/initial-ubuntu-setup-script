66e36499f7199144fece34468c807ab61dad9d06
简尚全 <jianshangquan3@gmail.com>
2024-10-15 16:27:17 +0630

    Create initial-setup.sh

---
+++ initial-setup.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt upgrade
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo passwd root
+sudo apt install glances speedtest-cli net-tools neofetch aptitude nmap python3 -y
+sudo aptitude safe-upgrade
+ufw disable
